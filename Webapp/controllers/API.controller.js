6abb5d6a4bc72a538d273cf2edef69abd4cc668b
Bryce Morrow <brycemorrow4564@gmail.com>
2017-09-24 20:54:26 -0400

    Fixed syntax err

--- Webapp/controllers/API.controller.js
+++ Webapp/controllers/API.controller.js
@@ -14,8 +14,7 @@ sap.ui.define([
 
         onInit: function() {
 
-            var urlPrefix = 'https://tranquil-bastion-27755.herokuapp.com/';
-//            var urlPrefix = 'http://localhost:8080',
+            var urlPrefix = 'https://tranquil-bastion-27755.herokuapp.com/',
                 apiObj = {
                     "API_functions" : [
                         {
