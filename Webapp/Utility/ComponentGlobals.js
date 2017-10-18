720fd8730696a937efe87b103b07c2071f771077
Bryce Morrow <brycemorrow4564@gmail.com>
2017-10-18 12:03:22 -0400

    Successfully removed ComponentGlobals to minimize imports and fixed issue with header border javascript

--- Webapp/Utility/ComponentGlobals.js
+++ Webapp/Utility/ComponentGlobals.js
@@ -1,46 +1,16 @@
-$.sap.require('sap.m.App');
-$.sap.require('sap.ui.core.mvc.View');
-$.sap.require('sap.ui.core.mvc.JSView');
 $.sap.require('sap.m.semantic.MasterPage');
 $.sap.require('sap.m.semantic.DetailPage');
-$.sap.require('sap.m.Label');
-$.sap.require('sap.m.Title');
-$.sap.require('sap.m.Page');
-$.sap.require('sap.m.Dialog');
-$.sap.require('sap.m.Button');
-$.sap.require('sap.m.SegmentedButton');
-$.sap.require('sap.ui.model.json.JSONModel');
-$.sap.require('sap.m.List');
-$.sap.require('sap.m.StandardListItem');
-$.sap.require('sap.m.MessageToast');
 $.sap.require('sap.m.semantic.DetailPage');
-$.sap.require('sap.m.MultiComboBox');
-$.sap.require('sap.m.ComboBox');
-$.sap.require('sap.m.Toolbar');
-$.sap.require('sap.m.ToolbarSpacer');
-$.sap.require('sap.m.SearchField');
-$.sap.require('sap.ui.table.Table');
-$.sap.require('sap.ui.layout.Grid');
 $.sap.require('sap.ui.layout.SplitterLayoutData');
 
 var COMPONENT = {
-
-    "SplitApp"          : sap.m.SplitApp,
     "SplitterLayoutData": sap.ui.layout.SplitterLayoutData,
-    "View"              : sap.ui.core.mvc.View,
     "MasterPage"        : sap.m.semantic.MasterPage,
     "DetailPage"        : sap.m.semantic.DetailPage,
     "JSView"            : sap.ui.core.mvc.JSView,
-    "Label"             : sap.m.Label,
-    "Title"             : sap.m.Title,
-    "Button"            : sap.m.Button,
-    "SegmentedButton"   : sap.m.SegmentedButton,
-    "Page"              : sap.m.Page,
-    "Dialog"            : sap.m.Dialog,
-    "SearchField"       : sap.m.SearchField,
+
     "App"               : sap.m.App,
-    "Toolbar"           : sap.m.Toolbar,
-    "ToolbarSpacer"     : sap.m.ToolbarSpacer,
+
     "JSONModel"         : sap.ui.model.json.JSONModel,
     "List"              : sap.m.List,
     "StandardListItem"  : sap.m.StandardListItem,
@@ -48,15 +18,5 @@ var COMPONENT = {
     "HTML"              : sap.ui.core.HTML,
     "jsfragment"        : sap.ui.jsfragment,
     "DetailPage"        : sap.m.semantic.DetailPage,
-    "Table"             : sap.m.Table,
-    "Column"            : sap.m.Column,
-    "MultiComboBox"     : sap.m.MultiComboBox,
-    "ComboBox"          : sap.m.ComboBox,
-    "HeaderContainer"   : sap.m.HeaderContainer,
-    "HorizontalLayout"  : sap.ui.layout.HorizontalLayout,
-    "Grid"              : sap.ui.layout.Grid,
-    "VerticalLayout"    : sap.ui.layout.VerticalLayout,
-    "LayoutData"        : sap.ui.layout.LayoutData,
-    "GridData"          : sap.ui.layout.GridData,
-    "MessageToast"      : sap.m.MessageToast
+
 }
